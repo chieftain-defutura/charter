cfbda1acc6e414027cdcb513fbf134fcf25f425d
Subaa P <103409637+Subaa2128@users.noreply.github.com>
2023-10-13 15:47:24 +0530

    chore: hero slider

--- src/component/Slider/HeroSlider/index.tsx
+++ src/component/Slider/HeroSlider/index.tsx
@@ -1,98 +1,46 @@
 import React, { useState, useEffect } from "react";
-import HeroImgOne from "../../../assets/images/hero-img-1.png";
-import HeroImgTwo from "../../../assets/images/hero-img-2.png";
-import HeroImgThree from "../../../assets/images/hero-img-3.png";
+import SlideImgOne from "../../../assets/images/hero-img-1.png";
+import SlideImgTwo from "../../../assets/images/hero-img-2.jpg";
+import SlideImgThree from "../../../assets/images/hero-img-3.png";
 import "./HeroSlider.scss";
 
-const data = [HeroImgThree, HeroImgOne, HeroImgTwo, HeroImgThree, HeroImgOne];
-
 const HeroSlider: React.FC = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handlePreviousClick = () => {
-    setCurrentPage((currentPage - 1 + data.length) % data.length);
-  };
-
-  const handleNextClick = () => {
-    setCurrentPage((currentPage + 1) % data.length);
-  };
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(
-      () =>
-        setCurrentPage((c) => {
-          if (c === data.length - 2) return 1;
-
-          return c + 1;
-        }),
-      4000
-    );
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % 3); // Assuming you have 3 slides
+    }, 2000); // Auto-loop every second (1000 milliseconds)
 
     return () => {
       clearInterval(interval);
     };
   }, []);
   return (
-    <div className="heroslider-container">
-      <div className="heroslider-wrapper">
-        <div className="image-container prev" onClick={handlePreviousClick}>
-          <div className="image"></div>
-          <img src={data[currentPage - 1]} alt={` ${currentPage - 1}`} />
-        </div>
-        <div className="image-container current">
-          <img src={data[currentPage]} alt={` ${currentPage}`} />
-        </div>
-        <div className={`image-container next`} onClick={handleNextClick}>
-          <div className="image"></div>
-          <img src={data[currentPage + 1]} alt={` ${currentPage + 1}`} />
-        </div>
-      </div>
-
-      <div className="slick-dots">
-        <div
-          style={{
-            background: currentPage === 1 ? "white" : "gray",
-            padding: currentPage === 1 ? "8px" : "4px",
-            fontSize: "0px",
-            borderRadius: "50%",
-            margin: "4px",
-            width: "8px",
-          }}
-          key={1}
-          onClick={() => setCurrentPage(1)}
+    <section id="slider">
+      {["s1", "s2", "s3"].map((id, index) => (
+        <input
+          key={id}
+          type="radio"
+          name="slider"
+          id={id}
+          checked={currentSlide === index}
+        />
+      ))}
+      {[SlideImgOne, SlideImgTwo, SlideImgThree].map((imgSrc, index) => (
+        <label
+          key={`slide${index + 1}`}
+          htmlFor={`s${index + 1}`}
+          id={`slide${index + 1}`}
         >
-          {1}
-        </div>
-        <div
-          style={{
-            background: currentPage === 2 ? "white" : "gray",
-            padding: currentPage === 2 ? "8px" : "4px",
-            fontSize: "0px",
-            borderRadius: "50%",
-            margin: "4px",
-            width: "8px",
-          }}
-          key={2}
-          onClick={() => setCurrentPage(2)}
-        >
-          {2}
-        </div>
-        <div
-          style={{
-            background: currentPage === 3 ? "white" : "gray",
-            padding: currentPage === 3 ? "8px" : "4px",
-            fontSize: "0px",
-            borderRadius: "50%",
-            margin: "4px",
-            width: "8px",
-          }}
-          key={3}
-          onClick={() => setCurrentPage(3)}
-        >
-          {3}
-        </div>
-      </div>
-    </div>
+          <img
+            src={imgSrc}
+            alt=""
+            style={{ objectFit: "cover", objectPosition: "top" }}
+          />
+        </label>
+      ))}
+    </section>
   );
 };
 

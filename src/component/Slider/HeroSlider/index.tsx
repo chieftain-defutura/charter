b0c4c2961abea48e5d1afe093fb2a69daf3fa8b7
suganya05 <101466647+suganya05@users.noreply.github.com>
2023-10-13 17:21:43 +0530

    chore: slider updated

--- src/component/Slider/HeroSlider/index.tsx
+++ src/component/Slider/HeroSlider/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import SlideImgOne from "../../../assets/images/hero-img-1.png";
-import SlideImgTwo from "../../../assets/images/hero-img-2.jpg";
+import SlideImgTwo from "../../../assets/images/hero-img-2.png";
 import SlideImgThree from "../../../assets/images/hero-img-3.png";
 import "./HeroSlider.scss";
 
